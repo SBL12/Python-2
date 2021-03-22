c10b9c42806b481e05cc2efb47b594de4034b78a
Suyash L <suyashblal@gmail.com>
2021-03-22 21:06:26 +0000

    Typing X, Adding Print X.

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ Name = input("What is your name freind?")
 print("Hello" + Name + "To coding Class")
 
 #for loop 
-for i in range(3)
+for i in range(3):
   print("Welcome")
   print ("Random")
+
+#typing X 
+  X = 50
+  print ("X")
+  print (type (X) )
