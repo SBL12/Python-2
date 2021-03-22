c36176131168c62f69ad3679e037259989ba95ac
Suyash L <suyashblal@gmail.com>
2021-03-22 21:39:53 +0000

    Added a number thing that tells the value of the number.

--- main.py
+++ main.py
@@ -12,3 +12,25 @@ for i in range(3):
   X = 50
   print ("X")
   print (type (X) )
+
+#Asking a number from the user: Even Or odd
+
+
+
+number = int(input("Enter Number to pass."))
+#checking number E or O
+if (number % 2) == 0:
+ print ("Number is... EVEN!")
+else:
+  print("Number is... ODD!")
+
+    #New Code: Write a program that takes a number and tells the user if the
+#number is greater than 10, less than 10, or equal to 10. Don't
+#forget to convert the string into an integer.
+
+
+ number2 = int(input("Enter Number another number:"))
+if (number2)> 10:
+    print("Number is greater than... 10!")
+elif number2< 10:
+    print("Number is less than 10!")
